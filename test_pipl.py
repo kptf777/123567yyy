1f0917d2a88519c01716aa7ccb7c6e146ef7b968
kptf777 <34883910+kptf777@users.noreply.github.com>
2025-02-13 16:50:13 +0900

    Update test_pipl.py

--- test_pipl.py
+++ test_pipl.py
@@ -43,4 +43,4 @@ class Pipeline:
             self, user_message: str, model_id: str, messages: List[dict], body: dict
     ) -> Union[str, Generator, Iterator]:
         # This is where you can add your custom pipelines like RAG.
-    return user_message
+        return user_message
