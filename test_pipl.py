37e06f049f8cc34886b0df578e5694a36548b85e
kptf777 <34883910+kptf777@users.noreply.github.com>
2025-02-13 16:42:45 +0900

    Update test_pipl.py

--- test_pipl.py
+++ test_pipl.py
@@ -37,9 +37,8 @@ class Pipeline:
         pass
 
     def pipe(
-            self, user_message: str, model_id: str, messages: List[dict], body: dict) -> Union[str, Generator, Iterator]:
-                # This is where you can add your custom pipelines like RAG.
-               # This is where you can add your custom pipelines like RAG.
+            self, user_message: str, model_id: str, messages: List[dict], body: dict
+    ) -> Union[str, Generator, Iterator]:
         print(f"pipe:{__name__}")
 
         print(messages)
