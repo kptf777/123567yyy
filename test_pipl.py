b39abed172875aad14620e2d7dd76b593e5d7d58
kptf777 <34883910+kptf777@users.noreply.github.com>
2025-02-13 16:43:07 +0900

    Update test_pipl.py

--- test_pipl.py
+++ test_pipl.py
@@ -36,9 +36,10 @@ class Pipeline:
         print(f"on_shutdown:{__name__}")
         pass
 
-    def pipe(
+   def pipe(
             self, user_message: str, model_id: str, messages: List[dict], body: dict
     ) -> Union[str, Generator, Iterator]:
+        # This is where you can add your custom pipelines like RAG.
         print(f"pipe:{__name__}")
 
         print(messages)
@@ -48,7 +49,39 @@ class Pipeline:
             "api-key": self.valves.AZURE_OPENAI_API_KEY,
             "Content-Type": "application/json",
         }
-        input = {"input":user_message}
-        response=[]
-  
-        return response
+
+        url = f"{self.valves.AZURE_OPENAI_ENDPOINT}/openai/deployments/{self.valves.AZURE_OPENAI_DEPLOYMENT_NAME}/chat/completions?api-version={self.valves.AZURE_OPENAI_API_VERSION}"
+
+        allowed_params = {'messages', 'temperature', 'role', 'content', 'contentPart', 'contentPartImage',
+                          'enhancements', 'data_sources', 'n', 'stream', 'stop', 'max_tokens', 'presence_penalty',
+                          'frequency_penalty', 'logit_bias', 'user', 'function_call', 'functions', 'tools',
+                          'tool_choice', 'top_p', 'log_probs', 'top_logprobs', 'response_format', 'seed'}
+        # remap user field
+        if "user" in body and not isinstance(body["user"], str):
+            body["user"] = body["user"]["id"] if "id" in body["user"] else str(body["user"])
+        filtered_body = {k: v for k, v in body.items() if k in allowed_params}
+        # log fields that were filtered out as a single line
+        if len(body) != len(filtered_body):
+            print(f"Dropped params: {', '.join(set(body.keys()) - set(filtered_body.keys()))}")
+
+        # Initialize the response variable to None.
+        r = None
+        try:
+            r = requests.post(
+                url=url,
+                json=filtered_body,
+                headers=headers,
+                stream=True,
+            )
+
+            r.raise_for_status()
+            if body["stream"]:
+                return r.iter_lines()
+            else:
+                return r.json()
+        except Exception as e:
+            if r:
+                text = r.text
+                return f"Error: {e} ({text})"
+            else:
+                return f"Error: {e}"
